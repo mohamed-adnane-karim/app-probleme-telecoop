2ff707407ac8963f49092f87f05cb784080b209b
mohamed-adnane-karim <mohamed.adnane.karim@gmail.com>
2024-06-13 16:10:50 +0200

    r

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,13 @@ import pageBugs from './components/page-bugs.vue';
 import pageAutomatiseeApi from './components/page-automatisee-api.vue';
 import pageAccueilAutomatisee from './components/page-accueil-automatisee.vue';
 import pageAutresProblemesApi from './components/page-autres-problemes-api.vue';
-import moduleQualirepar from './components/module-qualirepar-api.vue'
 
 </script>
 
 
 <template>
-    <!-- <div>
+    <div>
         <router-view></router-view>
-    </div> -->
-    <module-qualirepar/>
+    </div>
 </template>
 
